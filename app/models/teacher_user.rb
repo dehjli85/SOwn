a50b77301f2b9881de74895e548fbbd66aee454b
Dennis Li <dennis.hj.li@gmail.com>
2015-05-26 17:24:44 -0700

    added profile update code

--- app/models/teacher_user.rb
+++ app/models/teacher_user.rb
@@ -1,16 +1,28 @@
 class TeacherUser < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
-		
+    
+    puts 'AUTH_HASH:\n' + auth.to_s
 
+    #initialize the user if necessary
     where(auth.slice(:provider, :uid).to_hash).first_or_initialize do |user|
       user.provider = auth.provider
-      user.uid = auth.uid
-      user.display_name = auth.info.name
+      user.uid = auth.uid      
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
+
+    #create update the users profile info
+    retrieved_user = where(auth.slice(:provider, :uid).to_hash).first
+    retrieved_user.display_name = auth.info.name
+    retrieved_user.first_name = auth.info.given_name
+    retrieved_user.last_name = auth.info.family_name
+    retrieved_user.email = auth.info.email
+    retrieved_user.username = auth.info.email
+    retrieved_user.save!
+
+    retrieved_user
 
   end
 end
