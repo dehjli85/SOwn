7d1cfabefc3706fb694dce1971fede8b0073e2aa
Dennis Li <dennis.hj.li@gmail.com>
2015-07-13 18:16:26 -0700

    google oauth changes and teacher home page changes

--- app/models/teacher_user.rb
+++ app/models/teacher_user.rb
@@ -19,18 +19,18 @@ class TeacherUser < ActiveRecord::Base
     # end
 
     #initialize the user if necessary
-    where(auth.slice(:provider, :uid).to_hash).first_or_initialize do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid      
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.email = auth.info.email
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.username = auth.info.email
-      user.display_name = auth.info.name
+    user = TeacherUser.new
+    user.provider = auth.provider
+    user.uid = auth.uid      
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user.email = auth.info.email
+    user.first_name = auth.info.first_name
+    user.last_name = auth.info.last_name
+    user.username = auth.info.email
+    user.display_name = auth.info.name
       
-    end
+  
 
     return user
     
