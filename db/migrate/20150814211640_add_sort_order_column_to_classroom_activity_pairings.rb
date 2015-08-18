b1a2f9d7242564841b6019937be73156c78bfbb7
Dennis Li <dennis.hj.li@gmail.com>
2015-08-17 18:00:32 -0700

    migration change to fix sort order problem

--- db/migrate/20150814211640_add_sort_order_column_to_classroom_activity_pairings.rb
+++ db/migrate/20150814211640_add_sort_order_column_to_classroom_activity_pairings.rb
@@ -3,25 +3,6 @@ class AddSortOrderColumnToClassroomActivityPairings < ActiveRecord::Migration
 
   	add_column :classroom_activity_pairings, :sort_order, :integer
 
-  	classroom_ids = ClassroomActivityPairing.all.select("classroom_id").distinct.pluck(:classroom_id)
-
-  	classroom_ids.each do |id|
-
-  		caps = ClassroomActivityPairing.where({classroom_id: id}).order("created_at ASC")
-
-  		caps.each_with_index do |cap, index|
-
-  			cap.sort_order = index
-  			if(!cap.save)
-  				cap.errors.each do |k,v|
-  				end
-  			end
-
-  		end
-
-  	end
-
-
   	
 
   end
