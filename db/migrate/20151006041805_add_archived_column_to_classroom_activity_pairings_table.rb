8b69486df62bd8d277eb71ca41f70d14d445a471
Dennis Li <dennis@sowntogrow.com>
2015-10-05 21:25:07 -0700

    fixed migration so archived cannot be a null field

--- db/migrate/20151006041805_add_archived_column_to_classroom_activity_pairings_table.rb
+++ db/migrate/20151006041805_add_archived_column_to_classroom_activity_pairings_table.rb
@@ -1,6 +1,7 @@
 class AddArchivedColumnToClassroomActivityPairingsTable < ActiveRecord::Migration
   def change
 
-  	add_column :classroom_activity_pairings, :archived, :boolean
+  	add_column :classroom_activity_pairings, :archived, :boolean, null: false, default: false
+
   end
 end
