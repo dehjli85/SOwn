328790f8fa96c8bc32e8225b2100e509ed7bfec9
Dennis Li <dennis@sowntogrow.com>
2015-10-13 15:04:45 -0700

    migration for activity_goals table added

---
+++ db/migrate/20151013220223_create_activity_goals_table.rb
@@ -0,0 +1,10 @@
+class CreateActivityGoalsTable < ActiveRecord::Migration
+  def change
+    create_table :activity_goals_tables do |t|
+    	t.integer :classroom_activity_pairing_id
+    	t.integer :student_user_id
+    	t.float :score_goal
+    	t.datetime :goal_date
+    end
+  end
+end
