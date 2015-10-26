a044b6a079df7d0083cd0ffa117dd096085ed422
Dennis Li <dennis@sowntogrow.com>
2015-10-26 11:04:49 -0700

    updated homepage text, remove video hacks

--- app/assets/javascripts/public_pages/views/SignUpAndLogin_Home_View.js
+++ app/assets/javascripts/public_pages/views/SignUpAndLogin_Home_View.js
@@ -19,26 +19,8 @@ PublicPages.module("SignUpAndLoginApp.Home", function(Home, PublicPages, Backbon
 			},
 
 			events: {
-				"click @ui.videoDiv1": "playVideo1",
-				"click @ui.videoDiv2": "playVideo2",
-				"hover @ui.videoDiv1": "changePlayDivColors"
 			},
 
-			playVideo1: function(e){
-				e.preventDefault();
-				this.ui.videoDiv1.removeClass("video");
-				this.ui.videoDiv1.html('<iframe src="https://player.vimeo.com/video/142544815?autoplay=true" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
-			},
-
-			playVideo2: function(e){
-				e.preventDefault();
-				this.ui.videoDiv2.removeClass("video");
-				this.ui.videoDiv2.html('<iframe src="https://player.vimeo.com/video/142545484?autoplay=true" width="854" height="408" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>');
-			},
-
-			changePlayDivColors: function(e){
-				console.log("hello");
-			}
 			
 	});
 
